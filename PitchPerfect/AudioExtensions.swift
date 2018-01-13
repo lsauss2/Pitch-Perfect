b596e812000a2261366f93f16a2bf3591f22ec58
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-13 19:14:12 +0800

    Project finished

---
+++ PitchPerfect/AudioExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  AudioExtensions.swift
+//  PitchPerfect
+//
+//  Created by Ludovic Saussinan on 13/1/18.
+//  Copyright © 2018 Ludovic Saussinan. All rights reserved.
+//
+
+import Foundation
